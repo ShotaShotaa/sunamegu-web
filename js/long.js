f6e3d987b8b16ab92aff7d0ac2531c8879080254
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-04-10 18:06:38 +0900

    update 4/9

---
+++ js/long.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 22; i++) {
+    document.write('<div class="col-lo-4 col-sm-6">')
+    document.write(`<img src="./images/4-9/long/image (${i}).jpg" alt="アルバイト情報">`)
+    document.write('</div>')
+}
+
+for (let i = 1; i <= 19; i++) {
+    document.write('<div class="col-lo-4 col-sm-6">')
+    document.write(`<img src="./images/4-2/long/image (${i}).jpg" alt="アルバイト情報">`)
+    document.write('</div>')
+}
---
+++ js/volunteer.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 4; i++) {
+    document.write('<div class="col-lo-4 col-sm-6">')
+    document.write(`<img src="./images/volunteer/image (${i}).jpg" alt="アルバイト情報">`)
+    document.write('</div>')
+}
+
+// for (let i = 1; i <= 2; i++) {
+//     document.write('<div class="col-lo-4 col-sm-6">')
+//     document.write(`<img src="./images/4-2/short/image (${i}).jpg" alt="アルバイト情報">`)
+//     document.write('</div>')
+// }
