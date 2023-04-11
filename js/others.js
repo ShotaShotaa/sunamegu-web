7f32f00ab2546b948e1f2c89b7cd8d34457963be
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-04-11 14:23:05 +0900

    alt update

--- js/others.js
+++ js/others.js
@@ -6,6 +6,6 @@ for (let i = 1; i <= 1; i++) {
 
 // for (let i = 1; i <= 2; i++) {
 //     document.write('<div class="col-lo-4 col-sm-6">')
-//     document.write(`<img src="./images/4-2/short/image (${i}).jpg" alt="アルバイト情報">`)
+//     document.write(`<img src="./images/4-2/short/image (${i}).jpg" alt="その他のイベント">`)
 //     document.write('</div>')
 // }
