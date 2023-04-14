6c44cae0223154316ddff975d88ff2dc82d0916a
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-04-15 00:25:12 +0900

    Add files via upload

--- js/long.js
+++ js/long.js
@@ -1,7 +1,8 @@
 for (let i = 1; i <= 33; i++) {
     const text = document.getElementById('long');
     text.insertAdjacentHTML('afterbegin', `<div class="col-lo-4 col-sm-6">
-    <picture><source srcset="./images/4-9/long/image (${i}).webp" media="(max-width:500px)" type="image/webp">
+    <picture>
+    <source srcset="./images/4-9/long/image (${i}).webp" media="(max-width:500px)" type="image/webp">
     <source srcset="./images/4-9/long/image (${i}).jpg" media="(max-width:500px)">
     <source srcset="./images/4-9/long/image (${i}).webp" type="image/webp">
     <img src="./images/4-9/long/image (${i}).jpg" alt="アルバイト情報">
