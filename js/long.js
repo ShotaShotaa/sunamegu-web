cacb23df0ad84c967afae312c1a94fe96040d20f
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-04-17 21:41:33 +0900

    Add files via upload

--- js/long.js
+++ js/long.js
@@ -1,4 +1,40 @@
-for (let i = 1; i <= 33; i++) {
+for (let i = 1; i <= 22; i++) {
+    // 要素の作成
+const div = document.createElement('div');
+div.classList.add('col-lo-4', 'col-sm-6');
+
+const picture = document.createElement('picture');
+
+const source1 = document.createElement('source');
+source1.setAttribute('srcset', `./images/4-16/long/image (${i}).webp`);
+source1.setAttribute('media', '(max-width:500px)');
+source1.setAttribute('type', 'image/webp');
+picture.appendChild(source1);
+
+const source2 = document.createElement('source');
+source2.setAttribute('srcset', `./images/4-16/long/image (${i}).jpg`);
+source2.setAttribute('media', '(max-width:500px)');
+picture.appendChild(source2);
+
+const source3 = document.createElement('source');
+source3.setAttribute('srcset', `./images/4-16/long/image (${i}).webp`);
+source3.setAttribute('type', 'image/webp');
+picture.appendChild(source3);
+
+const img = document.createElement('img');
+img.setAttribute('src', `./images/4-16/long/image (${i}).jpg`);
+    img.setAttribute('alt', 'アルバイト情報');
+    img.setAttribute('loading', 'lazy');
+picture.appendChild(img);
+
+div.appendChild(picture);
+
+// DOMに要素を追加
+const target = document.getElementById('long'); // HTMLにある要素のIDを指定
+target.appendChild(div);
+}
+
+for (let i = 1; i <= 31; i++) {
     // 要素の作成
 const div = document.createElement('div');
 div.classList.add('col-lo-4', 'col-sm-6');
@@ -33,6 +69,7 @@ div.appendChild(picture);
 const target = document.getElementById('long'); // HTMLにある要素のIDを指定
 target.appendChild(div);
 }
+
 
 for (let i = 1; i <= 12; i++) {    // 要素の作成
 const div = document.createElement('div');
--- js/short.js
+++ js/short.js
@@ -1,16 +1,41 @@
-// for (let i = 1; i <= 3; i++) {
-//     document.write('<div class="col-lo-4 col-sm-6">')
-//     document.write(`<img src="./images/4-9/short/image (${i}).jpg" alt="アルバイト情報">`)
-//     document.write('</div>')
-// }
-
-// for (let i = 1; i <= 0; i++) {
-//     document.write('<div class="col-lo-4 col-sm-6">')
-//     document.write(`<img src="./images/4-2/short/image (${i}).jpg" alt="アルバイト情報">`)
-//     document.write('</div>')
-// }
-
 for (let i = 1; i <= 3; i++) {
+        // 要素の作成
+const div = document.createElement('div');
+div.classList.add('col-lo-4', 'col-sm-6');
+
+const picture = document.createElement('picture');
+
+const source1 = document.createElement('source');
+source1.setAttribute('srcset', `./images/4-16/short/image (${i}).webp`);
+source1.setAttribute('media', '(max-width:500px)');
+source1.setAttribute('type', 'image/webp');
+picture.appendChild(source1);
+
+const source2 = document.createElement('source');
+source2.setAttribute('srcset', `./images/4-16/short/image (${i}).jpg`);
+source2.setAttribute('media', '(max-width:500px)');
+picture.appendChild(source2);
+
+const source3 = document.createElement('source');
+source3.setAttribute('srcset', `./images/4-16/short/image (${i}).webp`);
+source3.setAttribute('type', 'image/webp');
+picture.appendChild(source3);
+
+const img = document.createElement('img');
+img.setAttribute('src', `./images/4-16/short/image (${i}).jpg`);
+img.setAttribute('alt', '短期バイト');
+    img.setAttribute('loading', 'lazy');
+picture.appendChild(img);
+
+div.appendChild(picture);
+
+// DOMに要素を追加
+const target = document.getElementById('short'); // HTMLにある要素のIDを指定
+target.appendChild(div);
+
+}
+
+for (let i = 1; i <= 2; i++) {
         // 要素の作成
 const div = document.createElement('div');
 div.classList.add('col-lo-4', 'col-sm-6');
