d5c92d81471bfb882ab2046173a9c0119a112853
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-04-19 11:21:03 +0900

    Add files via upload

--- js/footerFixed.js
+++ js/footerFixed.js
@@ -61,13 +61,3 @@ new function(){
 	addEvent(window,"resize",footerFixed);
 
 }
-
-// 以下１行を追加
-$(document).on('turbolinks:load', function() {
-  $(function () {
-    $('.alert_btn').click(function(){
-      alert('Hello World!')
-    });
-  });
-// 閉じタグも忘れず追加
-});
