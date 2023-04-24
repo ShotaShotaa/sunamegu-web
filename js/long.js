a78b1cd918e6528b09973c8b2d0baeb5d5078cde
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-04-24 18:04:50 +0900

    Add files via upload

--- js/long.js
+++ js/long.js
@@ -5,6 +5,27 @@ for (let i = 1; i <= 22; i++) {
 
     // img要素を作成する
     const img = document.createElement('img');
+    img.setAttribute('src', `images/4-24/long/image (${i}).webp`);
+    img.setAttribute('loading', 'lazy');
+    img.setAttribute('alt', 'アルバイト情報');
+
+    // div要素の子要素としてimg要素を追加する
+    div.appendChild(img);
+
+    // long IDを持つ要素を取得する
+    const longElement = document.getElementById('long');
+
+    // long要素の子要素としてdiv要素を追加する
+    longElement.appendChild(div);
+}
+
+for (let i = 1; i <= 21; i++) {
+    // div要素を作成する
+    const div = document.createElement('div');
+    div.classList.add('col-lo-4', 'col-sm-6');
+
+    // img要素を作成する
+    const img = document.createElement('img');
     img.setAttribute('src', `images/4-16/long/image (${i}).webp`);
     img.setAttribute('loading', 'lazy');
     img.setAttribute('alt', 'アルバイト情報');
@@ -19,7 +40,7 @@ for (let i = 1; i <= 22; i++) {
     longElement.appendChild(div);
 }
 
-for (let i = 1; i <= 31; i++) {
+for (let i = 1; i <= 27; i++) {
     // div要素を作成する
     const div = document.createElement('div');
     div.classList.add('col-lo-4', 'col-sm-6');
@@ -27,27 +48,6 @@ for (let i = 1; i <= 31; i++) {
     // img要素を作成する
     const img = document.createElement('img');
     img.setAttribute('src', `images/4-9/long/image (${i}).webp`);
-    img.setAttribute('loading', 'lazy');
-    img.setAttribute('alt', 'アルバイト情報');
-
-    // div要素の子要素としてimg要素を追加する
-    div.appendChild(img);
-
-    // long IDを持つ要素を取得する
-    const longElement = document.getElementById('long');
-
-    // long要素の子要素としてdiv要素を追加する
-    longElement.appendChild(div);
-}
-
-for (let i = 1; i <= 18; i++) {
-    // div要素を作成する
-    const div = document.createElement('div');
-    div.classList.add('col-lo-4', 'col-sm-6');
-
-    // img要素を作成する
-    const img = document.createElement('img');
-    img.setAttribute('src', `images/4-2/long/image (${i}).webp`);
     img.setAttribute('loading', 'lazy');
     img.setAttribute('alt', 'アルバイト情報');
 
