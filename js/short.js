c0cf5bb9c972e15deb47f14686718cba21c655f6
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-05-02 14:01:07 +0900

    Add files via upload

--- js/short.js
+++ js/short.js
@@ -1,4 +1,25 @@
-for (let i = 1; i <= 9; i++) {
+for (let i = 1; i <= 2; i++) {
+    // div要素を作成する
+    const div = document.createElement('div');
+    div.classList.add('col-lo-4', 'col-sm-6');
+
+    // img要素を作成する
+    const img = document.createElement('img');
+    img.setAttribute('src', `images/5-1/short/image (${i}).webp`);
+    img.setAttribute('loading', 'lazy');
+    img.setAttribute('alt', 'アルバイト情報');
+
+    // div要素の子要素としてimg要素を追加する
+    div.appendChild(img);
+
+    // long IDを持つ要素を取得する
+    const longElement = document.getElementById('short');
+
+    // long要素の子要素としてdiv要素を追加する
+    longElement.appendChild(div);
+}
+
+for (let i = 1; i <= 8; i++) {
     // div要素を作成する
     const div = document.createElement('div');
     div.classList.add('col-lo-4', 'col-sm-6');
@@ -27,27 +48,6 @@ for (let i = 1; i <= 1; i++) {
     // img要素を作成する
     const img = document.createElement('img');
     img.setAttribute('src', `images/4-16/short/image (${i}).webp`);
-    img.setAttribute('loading', 'lazy');
-    img.setAttribute('alt', 'アルバイト情報');
-
-    // div要素の子要素としてimg要素を追加する
-    div.appendChild(img);
-
-    // long IDを持つ要素を取得する
-    const longElement = document.getElementById('short');
-
-    // long要素の子要素としてdiv要素を追加する
-    longElement.appendChild(div);
-}
-
-for (let i = 1; i <= 2; i++) {
-    // div要素を作成する
-    const div = document.createElement('div');
-    div.classList.add('col-lo-4', 'col-sm-6');
-
-    // img要素を作成する
-    const img = document.createElement('img');
-    img.setAttribute('src', `images/4-9/short/image (${i}).webp`);
     img.setAttribute('loading', 'lazy');
     img.setAttribute('alt', 'アルバイト情報');
 
