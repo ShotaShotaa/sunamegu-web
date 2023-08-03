7fbabe9f15f3bd3fcbdf128fd4e868438c891a6c
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-08-03 19:06:49 +0900

    Add files via upload

---
+++ js/single.js
@@ -0,0 +1,20 @@
+for (let i = 1; i <= 4; i++) {
+  // div要素を作成する
+  const div = document.createElement("div");
+  div.classList.add("col-lo-4", "col-sm-6");
+
+  // img要素を作成する
+  const img = document.createElement("img");
+  img.setAttribute("src", `images/single/image (${i}).webp`);
+  img.setAttribute("loading", "lazy");
+  img.setAttribute("alt", "アルバイト情報");
+
+  // div要素の子要素としてimg要素を追加する
+  div.appendChild(img);
+
+  // long IDを持つ要素を取得する
+  const longElement = document.getElementById("single");
+
+  // long要素の子要素としてdiv要素を追加する
+  longElement.appendChild(div);
+}
