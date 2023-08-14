f2454cde4bfe80c180e67c889ffdcc777ea5b703
shoen-spl <128200720+shoen-spl@users.noreply.github.com>
2023-08-14 21:14:11 +0900

    Add files via upload

--- js/long.js
+++ js/long.js
@@ -1,3 +1,24 @@
+for (let i = 1; i <= 11; i++) {
+  // div要素を作成する
+  const div = document.createElement("div");
+  div.classList.add("col-lo-4", "col-sm-6");
+
+  // img要素を作成する
+  const img = document.createElement("img");
+  img.setAttribute("src", `images/8-14/long/image (${i}).webp`);
+  img.setAttribute("loading", "lazy");
+  img.setAttribute("alt", "アルバイト情報");
+
+  // div要素の子要素としてimg要素を追加する
+  div.appendChild(img);
+
+  // long IDを持つ要素を取得する
+  const longElement = document.getElementById("long");
+
+  // long要素の子要素としてdiv要素を追加する
+  longElement.appendChild(div);
+}
+
 for (let i = 1; i <= 17; i++) {
   // div要素を作成する
   const div = document.createElement("div");
@@ -27,27 +48,6 @@ for (let i = 1; i <= 10; i++) {
   // img要素を作成する
   const img = document.createElement("img");
   img.setAttribute("src", `images/7-31/long/image (${i}).webp`);
-  img.setAttribute("loading", "lazy");
-  img.setAttribute("alt", "アルバイト情報");
-
-  // div要素の子要素としてimg要素を追加する
-  div.appendChild(img);
-
-  // long IDを持つ要素を取得する
-  const longElement = document.getElementById("long");
-
-  // long要素の子要素としてdiv要素を追加する
-  longElement.appendChild(div);
-}
-
-for (let i = 1; i <= 12; i++) {
-  // div要素を作成する
-  const div = document.createElement("div");
-  div.classList.add("col-lo-4", "col-sm-6");
-
-  // img要素を作成する
-  const img = document.createElement("img");
-  img.setAttribute("src", `images/7-24/long/image (${i}).webp`);
   img.setAttribute("loading", "lazy");
   img.setAttribute("alt", "アルバイト情報");
 
